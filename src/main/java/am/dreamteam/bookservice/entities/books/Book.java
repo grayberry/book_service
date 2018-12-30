9b82d56b66876fbdac792b9fe08e2253dc3071a9
Rafael Sargsyan <tambovflow@gmail.com>
2018-12-31 01:46:32 +0400

    add Book.java class

---
+++ src/main/java/am/dreamteam/bookservice/entities/books/Book.java
@@ -0,0 +1,79 @@
+package am.dreamteam.bookservice.entities.books;
+
+public class Book {
+    private int id;
+    private String title;
+    private String language;
+    private int pageCount;
+    private String imageRef;
+    private String text;
+    private String isbn10;
+    private String isbn13;
+
+    public Book() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public int getPageCount() {
+        return pageCount;
+    }
+
+    public void setPageCount(int pageCount) {
+        this.pageCount = pageCount;
+    }
+
+    public String getImageRef() {
+        return imageRef;
+    }
+
+    public void setImageRef(String imageRef) {
+        this.imageRef = imageRef;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getIsbn10() {
+        return isbn10;
+    }
+
+    public void setIsbn10(String isbn10) {
+        this.isbn10 = isbn10;
+    }
+
+    public String getIsbn13() {
+        return isbn13;
+    }
+
+    public void setIsbn13(String isbn13) {
+        this.isbn13 = isbn13;
+    }
+}
