7c46323d8a627a4cb4450bbd49e2553346002800
hulkenberg7 <hulkenberg37@gmail.com>
2018-12-31 01:22:32 +0400

    modified Author.java

--- src/main/java/am/dreamteam/bookservice/entities/books/Author.java
+++ src/main/java/am/dreamteam/bookservice/entities/books/Author.java
@@ -1,4 +1,25 @@
 package am.dreamteam.bookservice.entities.books;
 
 public class Author {
+    private int id;
+    private String fullName;
+
+    public Author() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
 }
